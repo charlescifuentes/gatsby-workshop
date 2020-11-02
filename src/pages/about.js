e20da53c1dc759af48567fffc8ecf2db60ce01e9
Charles Cifuentes <charlescifuentes@gmail.com>
2020-11-02 13:14:42 -0500

    Add Header component and new features to index and about pages

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "React"
+import { Link } from "gatsby"
+import Header from "../components/header"
+
+export default About => {
+    return (
+        <div style={{ color: `teal` }}>
+            <Link to="/">Home</Link>
+            <Header title="About Us" />
+            <p>Such wow. Very React.</p>
+      </div>
+    )
+}
+
